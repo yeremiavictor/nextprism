06f7264db543b24eccc03c958258c396fe24ce8d
yeremiavictor <yeremiavictor@gmail.com>
2023-05-02 15:26:50 +0700

    init products/page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { PrismaClient } from "@prisma/client"
+import AddProduct from "./addProduct"
+const prisma = new PrismaClient()
 
-function Product() {
+const getProducts = async() => {
+    const res = await prisma.product.findMany({
+        select:{
+            id:true,
+            title:true,
+            price:true,
+            brandId: true,
+            brand:true,
+        }
+    })
+    return res
+}
+
+const getBrands = async() => {
+    const res = await prisma.brand.findMany()
+    return res
+}
+
+const Product = async() => {
+    const [products,brands] = await Promise.all([getProducts(), getBrands()]);
+
   return (
     <div>
+        <div className="mb-2">
+            <AddProduct brands={brands}/>
+        </div>
       <table className='table w-full'>
         <thead>
             <tr>
@@ -14,13 +39,15 @@ function Product() {
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
+            {products.map((product,index)=>(
+                <tr key={product.id}>
+                    <td>{index+1}</td>
+                    <td>{product.title}</td>
+                    <td>{product.price}</td>
+                    <td>{product.brand.name}</td>
+                    <td></td>
+                </tr>
+            ))}
         </tbody>
       </table>
     </div>
