4e0a9c5502f23eac57cd599bab5d48a5501a8077
yeremiavictor <yeremiavictor@gmail.com>
2023-05-02 14:30:39 +0700

    init products/page.tsx

---
+++ app/products/page.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Product() {
+  return (
+    <div>
+      <table className='table w-full'>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Product Name</th>
+                <th>Price</th>
+                <th>Brand</th>
+                <th className='text-center'>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Product
